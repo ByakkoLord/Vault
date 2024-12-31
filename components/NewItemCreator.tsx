a0a2679d72dbc594ee842ada0837fa6d957da938
ByakkoLord <g.fonsecadacosta@gmail.com>
2024-12-31 13:51:15 -0300

    add new inputs with style

--- components/NewItemCreator.tsx
+++ components/NewItemCreator.tsx
@@ -1,6 +1,6 @@
 import { TextInput, View } from "react-native";
 import { StyleSheet } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
+import { LinearGradient } from "expo-linear-gradient";
 import PropTypes from 'prop-types';
 import { useState } from "react";
 
@@ -13,13 +13,39 @@ export default function NewItem({state}: stateProps) {
 
     return(
         
-        <LinearGradient colors={['black', 'white']} style={[styles.main, {display: state ? 'flex' : 'none'}]}>
+        <LinearGradient colors={['#2B2727', '#2B2727', '#B93254']} style={[styles.main, {display: state ? 'flex' : 'none'}]}>
         <TextInput
-          placeholder="Name"
+          placeholder="Nome"
           editable={true}
-          style={styles.input1}
+          style={[styles.input, {width: 300, height: 50,}]}
           placeholderTextColor="gray"
         />
+        <TextInput
+          placeholder="Nome"
+          editable={true}
+          style={[styles.input, {width: 300, height: 50,}]}
+          placeholderTextColor="gray"
+        />
+        <TextInput
+          placeholder="Nome"
+          editable={true}
+          style={[styles.input, {width: 300, height: 50,}]}
+          placeholderTextColor="gray"
+        />
+        <View style={{display: 'flex', flexDirection: 'row', width: 320, justifyContent: 'space-around'}}>
+        <TextInput
+          placeholder="1X"
+          editable={true}
+          style={[styles.input, {width: 80, height: 50,}]}
+          placeholderTextColor="gray"
+        />
+        <TextInput
+          placeholder="1X"
+          editable={true}
+          style={[styles.input, {width: 200, height: 50,}]}
+          placeholderTextColor="gray"
+        />  
+        </View>
       </LinearGradient>
         
     )
@@ -30,19 +56,20 @@ const styles = StyleSheet.create({
         position: 'absolute',
         backgroundColor: 'black, white',
         width: 350,
-        top: 10,
-        height: 600,
+        top: 100,
+        height: 330,
         borderRadius: 10,
         marginBottom: 20,
         alignItems: 'center',
         zIndex: 100
     },
-    input1: {
-        position: 'absolute',
-        width: 300,
-        height: 90,
+    input: {
+        paddingLeft: 20,
+        marginTop: 25,
+        borderRadius: 10,
+        fontSize: 20,
         color: 'black',
-        backgroundColor: 'white'
+        backgroundColor: '#231F1F'
     }
 })
 
